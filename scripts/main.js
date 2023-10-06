cc2911c22a721bd5f1ff34efadbafcd9e1027a69
QA-Nirivniy <147070821+QA-Nirivniy@users.noreply.github.com>
2023-10-06 15:01:36 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ var myImage = document.querySelector("img");
 
 myImage.onclick = function () {
   var mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-icon.png") {
+  if (mySrc === "images/images.jpeg") {
     myImage.setAttribute("src", "images/123.jpg");
   } else {
     myImage.setAttribute("src", "images/images.jpeg");
